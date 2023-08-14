3d69cabb9f48d1334c9d77aea0645a7b5673ecb2
bhavesh <bsuthar7073@gmail.com>
2023-08-14 23:13:18 +0530

    1st Commit

--- app/src/main/java/com/example/contacts/ContactDetail.java
+++ app/src/main/java/com/example/contacts/ContactDetail.java
@@ -2,13 +2,19 @@ package com.example.contacts;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.database.Cursor;
 import android.net.Uri;
 import android.os.Bundle;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -19,7 +25,15 @@ public class ContactDetail extends AppCompatActivity {
     Uri newRowId;
     TextView nameTextView;
     TextView numTextView;
+    String num;
     private Uri mContactUri;
+
+    // Variable Declaration
+
+    private EditText number_input;
+
+    //Request Code
+    private int request_Code = 101;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -50,7 +64,7 @@ public class ContactDetail extends AppCompatActivity {
             while (cursor.moveToNext()) {
                 String currName = cursor.getString(nameIndex);
                 String curNum = cursor.getString(numIndex);
-
+                num = curNum;
                 nameTextView.append(currName);
                 numTextView.append(curNum);
 
@@ -80,6 +94,41 @@ public class ContactDetail extends AppCompatActivity {
         startActivity(intent);
     }
 
+    public void callButton(View view){
+        int check_permission = ContextCompat.checkSelfPermission(ContactDetail.this, Manifest.permission.CALL_PHONE);
+
+        if (check_permission == PackageManager.PERMISSION_GRANTED) {
+            makePhoneCall();
+        } else {
+            ActivityCompat.requestPermissions(ContactDetail.this, new String[]{Manifest.permission.CALL_PHONE}, request_Code);
+        }
+    }
+
+    private void makePhoneCall() {
+
+        String phone_Number = num;
+
+        if (phone_Number.trim().length() > 0) {            // it will trim or dlt empty spaces
+
+            String dial = "tel:" + phone_Number;
+            startActivity(new Intent(Intent.ACTION_CALL, Uri.parse(dial)));
+        } else {
+            Toast.makeText(this, "Enter Phone Number", Toast.LENGTH_LONG).show();
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        switch (requestCode) {
+            case 77:
+                if (grantResults.length >= 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    makePhoneCall();
+                } else {
+                    Toast.makeText(this, "You Don't have permission", Toast.LENGTH_LONG).show();
+                }
+        }
+    }
 
     @Override
     public boolean onOptionsItemSelected(@NonNull MenuItem item) {
